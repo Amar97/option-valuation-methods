010e142d606c517dc6dddb6bfcddcd0759d08061
Amar97 <amarramdas97@gmail.com>
2018-10-04 22:32:19 +0200

    finished 1 and 2

--- customFunctions.m
+++ customFunctions.m
@@ -1,1 +1,19 @@
-test 1 2 3;
+clear all;
+
+r=0.15;
+assd(1)=4;
+d29(1)=4;
+T = 20;
+L=40;
+deltat = T/L;
+
+
+for i = 1:L % let run for L delta t periods
+   tval(i+1) = i + 1; % time in years 
+   assd(i+1) = (1 + r*(deltat^(1/2)))*assd(i); 
+   d29(i+1) = (1 + r*(deltat^(3/2)))*d29(i); 
+end
+
+plot(assd);
+hold on;
+plot(d29)
